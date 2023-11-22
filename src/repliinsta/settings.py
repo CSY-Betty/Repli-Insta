ba2e0ad5e9ddfb66c8901b5e480dc81e4b04367a
Betty <qwe50929@gmail.com>
2023-11-22 22:55:57 +0800

    feat: setting rds

--- src/repliinsta/settings.py
+++ src/repliinsta/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -99,8 +104,12 @@ WSGI_APPLICATION = "repliinsta.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "RepliInsta",
+        "USER": os.getenv("RDSuser"),
+        "PASSWORD": os.getenv("RDSpassword"),
+        "HOST": "replirds.ccek64hlwvob.ap-northeast-1.rds.amazonaws.com",
+        "PORT": 3306,
     }
 }
 
