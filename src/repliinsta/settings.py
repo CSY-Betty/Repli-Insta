9b25eac6c697193bc3311c55791bfffc237f50b3
Betty <qwe50929@gmail.com>
2023-12-07 15:09:24 +0800

    feat: delete storages new setting

--- src/repliinsta/settings.py
+++ src/repliinsta/settings.py
@@ -182,7 +182,7 @@ AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
 AWS_S3_SIGNATURE_NAME = ("s3v4",)
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-AWS_S3_VERITY = True
+AWS_S3_VERIFY = True
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_S3_CUSTOM_DOMAIN = "d18tf8tm5a95y6.cloudfront.net"
